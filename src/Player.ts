a682e312981a939a60d876bef0ca8e8d95b5784a
Magoninho <magoninhogamer@gmail.com>
2021-09-05 21:34:19 -0300

    made some tweaks (bad code eww)

--- src/Player.ts
+++ src/Player.ts
@@ -1,4 +1,4 @@
-class Player {
+class Player extends Mob {
 
 	x: number;
 	y: number;
@@ -9,10 +9,12 @@ class Player {
 	game: Game;
 	playerImage: HTMLImageElement;
 	isMoving: boolean;
+	playerCoordinates: Coordinates;
 
 	name: string;
 
 	constructor(game: Game, x: number, y: number) {
+		super(x, y, PLAYER_SIZE, PLAYER_SIZE);
 		this.x = x;
 		this.y = y;
 		this.dx = 0;
@@ -21,9 +23,11 @@ class Player {
 		this.playerImage = new Image(PLAYER_SIZE, PLAYER_SIZE);
 		this.playerImage.src = "../images/profile.png";
 		this.isMoving = false;
+
+		this.playerCoordinates = new Coordinates(10, 15, this);
 		
 		// this will make sure that the player won't spawn suffocated in a block
-		while (getTileAt(this.x, this.y, LEVEL_1) == 1) {
+		while (getTileAt(this.x, this.y, LEVEL_1) == 1 || getTileAt(this.x, this.y, LEVEL_1) == 3) {
 			// todo: change this later
 			this.x = Math.floor(Math.random() * WORLD_WIDTH/2) + 200;
 			this.y = Math.floor(Math.random() * WORLD_HEIGHT/2) + 200;
@@ -49,6 +53,8 @@ class Player {
 		this.dy = 0;
 	}
 
+
+
 	// TODO: fazer um array com walls e um loop pra testar colisão com cada um dentro dessa função
 	// TODO: renomear para isCollidingWithWalls pra fazer sentido essa alteração
 
@@ -67,26 +73,35 @@ class Player {
 		return false;
 	}
 
-	private applyTileCollisions(arr: Tile[]): boolean {
+	private isCollidingWithTiles(arr: Tile[]): boolean {
 		for (let index = 0; index < arr.length; index++) {
-			// if (arr[index]) {
-				if (this.x + (this.dx * 10) < arr[index].x + arr[index].width &&
-					this.x + (this.dx * 10) + PLAYER_SIZE > arr[index].x &&
-					this.y + (this.dy * 10) < arr[index].y + arr[index].height &&
-					this.y + (this.dy * 10) + PLAYER_SIZE > arr[index].y) {
-					arr[index].onCollisionEnter();
-					return true;
-				// }
+			if (this.x + (this.dx * 10) < arr[index].x + arr[index].width &&
+				this.x + (this.dx * 10) + PLAYER_SIZE > arr[index].x &&
+				this.y + (this.dy * 10) < arr[index].y + arr[index].height &&
+				this.y + (this.dy * 10) + PLAYER_SIZE > arr[index].y &&
+				arr[index].rigid) {
+				return true;
 			}
 		}
 		return false;
 	}
 
-	public update(deltaTime): void {
+	private applyTileCollisions(arr: Tile[]): void {
+		for (let index = 0; index < arr.length; index++) {
+			if (this.x + (this.dx * 10) < arr[index].x + arr[index].width &&
+				this.x + (this.dx * 10) + PLAYER_SIZE > arr[index].x &&
+				this.y + (this.dy * 10) < arr[index].y + arr[index].height &&
+				this.y + (this.dy * 10) + PLAYER_SIZE > arr[index].y) {
+				arr[index].onCollisionEnter();
+			}
+		}
+	}
+
+	public update(deltaTime: number): void {
 
 		if (this.isMoving) statsManager.energyDecrease();
 		
-		if (!this.isCollidingWithMob(game.mobList)) {
+		if (!this.isCollidingWithMob(game.mobList) && !this.isCollidingWithTiles(game.blocks)) {
 			this.vx = this.dx * PLAYER_SPEED * deltaTime;
 			this.vy = this.dy * PLAYER_SPEED * deltaTime;
 
@@ -113,11 +128,12 @@ class Player {
 		if (this.y < 0) this.y = 0;
 		if (this.y > this.game.level.getLevel().length * TILESIZE - PLAYER_SIZE) this.y = this.game.level.getLevel().length * TILESIZE - PLAYER_SIZE;
 
-
+		this.playerCoordinates.update();
 	}
 
 	public render(ctx: CanvasRenderingContext2D): void {
 		ctx.drawImage(this.playerImage, this.x, this.y, PLAYER_SIZE, PLAYER_SIZE);
+		this.playerCoordinates.render(ctx);
 	}
 
 	public setPosition(x: number, y: number) {
